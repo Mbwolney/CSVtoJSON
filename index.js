fc721057f6929477c773f9af7cc6c56bdc940c79
Mbwolney <50266026+Mbwolney@users.noreply.github.com>
2020-02-17 17:19:07 -0300

    melhoria ao colcoar informações na table
    - add table
    - reinstruturação do projeto

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ function read(input) {
 reader.onload = function(e) {
     dados = e.target.result;
 
-    document.getElementById("info").innerText = dados;
-    document.getElementById("btnDown").disabled = false;
-}
-
-function download() {
     //-> Converter CSV em Json
     var dadosplit = dados.split("\r\n");
     var pessoas = [];
@@ -31,10 +26,39 @@ function download() {
         pessoas.push(pessoa);
     }
 
-    //-> Criar arquivo para baixar
+    var table = document.getElementById("table");
+    var str = "<tr><th>N</th><th>Nome</th><th>Data Nascimento</th><th>CPF</th></tr>";
+
+    pessoas.forEach(function(pessoa, index) {
+        str += `<tr>
+        <td>${index + 1}</td>
+        <td>${pessoa.nome}</td>
+        <td>${pessoa.dataNascimento}</td>
+        <td>${pessoa.cpf}</td>
+        </tr>`;
+    });
+
+    table.innerHTML = str;
+
+    // Habilitar Botão
+    // document.getElementById("btnDown").disabled = false;
+}
+
+function download() {
     var elem = document.createElement('a');
     elem.href = 'data:text/json;charset=utf-8,' + encodeURI(JSON.stringify(pessoas));
     elem.target = '_blank';
     elem.download = 'pessoas.json';
     elem.click();
+}
+
+function selecionarArquivo() {
+    document.getElementById("iptFile").click();
+}
+
+function onDrop(evt) {
+    debugger
+    const data = event.dataTransfer.getData("text/csv");
+    event.target.textContent = data;
+    event.preventDefault();
 }
